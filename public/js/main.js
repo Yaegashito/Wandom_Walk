4b02a8fc4c44e55317e860df8e447c6bcfb43b19
tomoyae1007.xperia26@gmail.com <tomoyae1007.xperia26@gmail.com>
2024-07-10 13:53:56 +0900

    カレンダー等を書くための準備

--- public/js/main.js
+++ public/js/main.js
@@ -1,135 +1,139 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-    alert('失敗');
-}
+'use strict';
 
-function showPosition(position) {
-    let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
-    document.querySelector('#lat').value = lat;
-    document.querySelector('#lon').value = lng;
-    initMap(lat, lng);
-}
+{
+    // walk
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert('失敗');
+    }
 
-let map;
+    function showPosition(position) {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        document.querySelector('#lat').value = lat;
+        document.querySelector('#lon').value = lng;
+        initMap(lat, lng);
+    }
 
-function initMap(lat, lng) {
+    let map;
 
-    // 地図の初期設定
-    map = new google.maps.Map(document.getElementById('map'), {
-        // center: { lat: 35.6895, lng: 139.6917 }, // 東京の中心
-        center: new google.maps.LatLng(lat, lng),
-        zoom: 18,
-    });
+    function initMap(lat, lng) {
 
-    let marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat, lng),
-        map: map,
-        title: '現在地',
-    });
-}
+        // 地図の初期設定
+        map = new google.maps.Map(document.getElementById('map'), {
+            // center: { lat: 35.6895, lng: 139.6917 }, // 東京の中心
+            center: new google.maps.LatLng(lat, lng),
+            zoom: 18,
+        });
 
-let directionsService;
-let directionsRenderer;
-
-function renderRoute(points, waypoints, pickupIdx) {
-    map = new google.maps.Map(document.getElementById('map'), {
-        // center: { lat: 35.6895, lng: 139.6917 },
-        zoom: 8,
-    });
-
-    directionsService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer({
-        // suppressMarkers: true, //経由地のピンを非表示
-        polylineOptions: {
-            strokeColor: 'blue', // ポリラインの色を青に設定
-            strokeWeight: 2,      // ポリラインの太さを設定
-            icons: [{
-                icon: {
-                    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-                    scale: 3,
-                    strokeColor: 'red'
-                },
-                offset: '100%',
-                repeat: '100px'
-            }]
-        }
-    });
-    directionsRenderer.setMap(map);
-
-    // 他のAPIから取得した経由地の情報をここに設定
-    const waypointsT = [];
-
-    for (let i = 0; i < waypoints; i++) {
-        waypointsT.push({
-            location: { lat: parseFloat(points[pickupIdx[i]].lat), lng: parseFloat(points[pickupIdx[i]].lon) }
-            // `${points[pickupIdx[i]].lat},${points[pickupIdx[i]].lon}/`
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map: map,
+            title: '現在地',
         });
     }
 
-    const origin = waypointsT.shift().location;
-    const destination = waypointsT.pop().location;
+    let directionsService;
+    let directionsRenderer;
 
-    directionsService.route(
-        {
-            origin: origin,
-            destination: destination,
-            waypoints: waypointsT,
-            optimizeWaypoints: true,
-            travelMode: 'DRIVING',
-        },
-        (response, status) => {
-            if (status === 'OK') {
-                directionsRenderer.setDirections(response);
+    function renderRoute(points, waypoints, pickupIdx) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            // center: { lat: 35.6895, lng: 139.6917 },
+            zoom: 8,
+        });
 
-                const steps = response.routes[0].legs[0].steps;
-                steps.forEach((step, index) => {
-                    const marker = new google.maps.Marker({
-                        position: step.start_location,
-                        map: map,
-                        label: `${index + 1}`
-                    });
-
-                    const infowindow = new google.maps.InfoWindow({
-                        content: step.instructions
-                    });
-
-                    marker.addListener('click', () => {
-                        infowindow.open(map, marker);
-                    });
-                });
-
-                let totalDistance = 0;
-                const legs = response.routes[0].legs;
-                for (let i = 0; i < legs.length; i++) {
-                    totalDistance += legs[i].distance.value; // 距離をメートルで取得
-                }
-                totalDistance = totalDistance / 1000; // キロメートルに変換
-
-                console.log('Total Distance: ' + totalDistance + ' km');
-            } else {
-                window.alert('Directions request failed due to ' + status);
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer({
+            // suppressMarkers: true, //経由地のピンを非表示
+            polylineOptions: {
+                strokeColor: 'blue', // ポリラインの色を青に設定
+                strokeWeight: 2,      // ポリラインの太さを設定
+                icons: [{
+                    icon: {
+                        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                        scale: 3,
+                        strokeColor: 'red'
+                    },
+                    offset: '100%',
+                    repeat: '100px'
+                }]
             }
+        });
+        directionsRenderer.setMap(map);
+
+        // 他のAPIから取得した経由地の情報をここに設定
+        const waypointsT = [];
+
+        for (let i = 0; i < waypoints; i++) {
+            waypointsT.push({
+                location: { lat: parseFloat(points[pickupIdx[i]].lat), lng: parseFloat(points[pickupIdx[i]].lon) }
+                // `${points[pickupIdx[i]].lat},${points[pickupIdx[i]].lon}/`
+            });
         }
-    );
-}
 
-document.querySelector('#start').addEventListener('click', () => {
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        const origin = waypointsT.shift().location;
+        const destination = waypointsT.pop().location;
 
-    fetch('/map', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-CSRF-TOKEN': csrfToken
-        },
-        body: new URLSearchParams({
-            lat: document.querySelector('#lat').value,
-            lon: document.querySelector('#lon').value,
-            per: document.querySelector('#per').value,
-        }),
-    })
+        directionsService.route(
+            {
+                origin: origin,
+                destination: destination,
+                waypoints: waypointsT,
+                optimizeWaypoints: true,
+                travelMode: 'DRIVING',
+            },
+            (response, status) => {
+                if (status === 'OK') {
+                    directionsRenderer.setDirections(response);
+
+                    const steps = response.routes[0].legs[0].steps;
+                    steps.forEach((step, index) => {
+                        const marker = new google.maps.Marker({
+                            position: step.start_location,
+                            map: map,
+                            label: `${index + 1}`
+                        });
+
+                        const infowindow = new google.maps.InfoWindow({
+                            content: step.instructions
+                        });
+
+                        marker.addListener('click', () => {
+                            infowindow.open(map, marker);
+                        });
+                    });
+
+                    let totalDistance = 0;
+                    const legs = response.routes[0].legs;
+                    for (let i = 0; i < legs.length; i++) {
+                        totalDistance += legs[i].distance.value; // 距離をメートルで取得
+                    }
+                    totalDistance = totalDistance / 1000; // キロメートルに変換
+
+                    console.log('Total Distance: ' + totalDistance + ' km');
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            }
+        );
+    }
+
+    document.querySelector('#start').addEventListener('click', () => {
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        fetch('/map', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-CSRF-TOKEN': csrfToken
+            },
+            body: new URLSearchParams({
+                lat: document.querySelector('#lat').value,
+                lon: document.querySelector('#lon').value,
+                per: document.querySelector('#per').value,
+            }),
+        })
         .then(res => {
             return res.json();
         })
@@ -148,4 +152,11 @@ document.querySelector('#start').addEventListener('click', () => {
 
             document.getElementById('result').textContent = finalURL;
         });
-});
+    });
+
+    // calendar
+
+    // belongings
+
+    // config
+}
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -17,23 +17,34 @@
     </header>
 
     <main>
-        <div id="map"></div>
+        <div id="walk" class="container">
+            <div id="map"></div>
 
-        <input type="text" id="lat" placeholder="Latitude">
-        <input type="text" id="lon" placeholder="Longitude">
-        <input type="text" id="per" placeholder="Perimeter (km)">
-        <button id="start">Generate Route</button>
-        <div id="result"></div>
-        <h1>1</h1>
-        <h1>2</h1>
-        <h1>3</h1>
-        <h1>4</h1>
-        <h1>5</h1>
-        <h1>6</h1>
-        <h1>7</h1>
-        <h1>8</h1>
-        <h1>9</h1>
-        <h1>10</h1>
+            <input type="text" id="lat" placeholder="Latitude">
+            <input type="text" id="lon" placeholder="Longitude">
+            <input type="text" id="per" placeholder="Perimeter (km)">
+            <button id="start">Generate Route</button>
+            <div id="result"></div>
+            <h1>1</h1>
+            <h1>2</h1>
+            <h1>3</h1>
+            <h1>4</h1>
+            <h1>5</h1>
+            <h1>6</h1>
+            <h1>7</h1>
+            <h1>8</h1>
+            <h1>9</h1>
+            <h1>10</h1>
+        </div>
+        <div id="calendar" class="container">
+            <h1>カレンダー</h1>
+        </div>
+        <div id="belongings" class="container">
+            <h1>持ち物リスト</h1>
+        </div>
+        <div id="config" class="container">
+            <h1>設定</h1>
+        </div>
     </main>
 
     <footer>
@@ -44,10 +55,10 @@
             <li>設定</li>
         </ul>
     </footer>
-    {{-- <script
+    <script
         src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ $key }}&callback=initMap"
         async defer>
-    </script> --}}
+    </script>
     <script src="{{asset('js/main.js') }}"></script>
 </body>
 </html>
