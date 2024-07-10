a6f550280e53fe74187f0c78f7b2590587432d1a
tomoyae1007.xperia26@gmail.com <tomoyae1007.xperia26@gmail.com>
2024-07-10 11:36:40 +0900

    reset.css追加、レイアウトを多少

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,22 +5,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
-    <div id="map"></div>
+    <header>
+        <h1>LOGO</h1>
+        <h1>アプリ名</h1>
+    </header>
 
-    <input type="text" id="lat" placeholder="Latitude">
-    <input type="text" id="lon" placeholder="Longitude">
-    <input type="text" id="per" placeholder="Perimeter (km)">
-    <button id="start">Generate Route</button>
-    <div id="result"></div>
+    <main>
+        <div id="map"></div>
 
-    <script
+        <input type="text" id="lat" placeholder="Latitude">
+        <input type="text" id="lon" placeholder="Longitude">
+        <input type="text" id="per" placeholder="Perimeter (km)">
+        <button id="start">Generate Route</button>
+        <div id="result"></div>
+        <h1>1</h1>
+        <h1>2</h1>
+        <h1>3</h1>
+        <h1>4</h1>
+        <h1>5</h1>
+        <h1>6</h1>
+        <h1>7</h1>
+        <h1>8</h1>
+        <h1>9</h1>
+        <h1>10</h1>
+    </main>
+
+    <footer>
+        <ul>
+            <li>地図</li>
+            <li>カレンダー</li>
+            <li>持ち物リスト編集</li>
+            <li>設定</li>
+        </ul>
+    </footer>
+    {{-- <script
         src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ $key }}&callback=initMap"
-        async defer></script>
+        async defer>
+    </script> --}}
     <script src="{{asset('js/main.js') }}"></script>
 </body>
 </html>
