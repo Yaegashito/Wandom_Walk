7f7a31dfd3a04c1756037f5baaf5ac1c2b8661c9
tomoyae1007.xperia26@gmail.com <tomoyae1007.xperia26@gmail.com>
2024-07-17 16:21:36 +0900

    #12 GraphHopperとの併用からGoogleMapのみの仕様に変更

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -20,11 +20,13 @@
         <div id="walk" class="container">
             <div id="map"></div>
 
-            <input type="text" id="lat" placeholder="Latitude">
-            <input type="text" id="lon" placeholder="Longitude">
-            <input type="text" id="per" placeholder="Perimeter (km)">
-            <button id="start">Generate Route</button>
-            <div id="result"></div>
+            <form id="route-form">
+                <label for="distance">距離 (km):
+                    <input type="number" id="distance" name="distance" step="0.1" required>
+                </label>
+                <button type="submit">経路を生成</button>
+            </form>
+
             <h1>1</h1>
             <h1>2</h1>
             <h1>3</h1>
@@ -131,10 +133,14 @@
             <li>設定</li>
         </ul>
     </footer>
-    {{-- <script
-        src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ $key }}&callback=initMap"
-        async defer>
-    </script> --}}
+      <script>
+        (g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
+        key: "{{ $key }}",
+        v: "weekly",
+        // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+        // Add other bootstrap parameters as needed, using camel case.
+        });
+    </script>
     <script src="{{asset('js/main.js') }}"></script>
 </body>
 </html>
