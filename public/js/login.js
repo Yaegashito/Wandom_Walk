e1b48185bcc888ec25e599c2ec6040b5235d7804
Yaegashito <tomoyae1007.xperia26@gmail.com>
2024-10-01 15:39:46 +0900

    #32 login.jsもprettierで整形

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,22 @@
 "use strict";
 
 {
-    const tabs = document.querySelectorAll('.tabs li a');
-    const contents = document.querySelectorAll('.content');
-    tabs.forEach(tab => {
-        tab.addEventListener('click', (e) => {
+    const tabs = document.querySelectorAll(".tabs li a");
+    const contents = document.querySelectorAll(".content");
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", (e) => {
             e.preventDefault();
-            tabs.forEach(tab => {
-                tab.classList.remove('active');
+            tabs.forEach((tab) => {
+                tab.classList.remove("active");
             });
-            tab.classList.add('active');
+            tab.classList.add("active");
 
             contents.forEach((content) => {
                 content.classList.remove("active");
             });
-            document.querySelector(`#${tab.dataset.id}`).classList.add('active');
+            document
+                .querySelector(`#${tab.dataset.id}`)
+                .classList.add("active");
         });
     });
 }
