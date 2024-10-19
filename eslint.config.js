bd3c11eb1cd2f6c5c1a1ae4f6de9d286ed14d7ae
Yaegashito <tomoyae1007.xperia26@gmail.com>
2024-10-19 10:58:20 +0900

    #39 cyがeslintに検知されないように

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
-  {languageOptions: { globals: {
-    ...globals.browser,
-    ...globals.jest,
-}, }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        cy: "readonly",
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
