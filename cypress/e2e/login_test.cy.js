b51f98c67088b21062109bec35497909c46a695e
Yaegashito <tomoyae1007.xperia26@gmail.com>
2024-10-20 12:16:27 +0900

    #39 カレンダーの挙動をテスト

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -1,5 +1,8 @@
 describe("Login Test", () => {
-  before(() => {
+//   before(() => {
+//   });
+
+  beforeEach(() => {
     cy.visit("http://localhost:8000/login");
     cy.get("#name").type("hoge");
     cy.get("#password").type("hogehoge");
@@ -8,17 +11,13 @@ describe("Login Test", () => {
     cy.get("#map").should("be.visible");
   });
 
-  beforeEach(() => {
-    cy.on('window:confirm', () => {
-      return true;
-    });
-  });
-
   it("walking buttons functionality verification", () => {
     const generateRoute = (buttonText) => {
       cy.contains(".generate-route", buttonText).click();
       cy.contains("#messages", "kmの経路ができました。").should("be.visible");
     };
+
+    // 経路生成から散歩完了まで
     cy.get("#distance").select("1");
     cy.get("#distance").should("have.value", "1");
     generateRoute("経路を生成");
@@ -26,8 +25,23 @@ describe("Login Test", () => {
     cy.get("#decide-route").click();
     cy.get("#start-btn").click();
     cy.get("#finish-btn").click();
+
+    // 経路生成から散歩中止まで
     generateRoute("経路を生成");
     cy.get("#stop-btn").click();
     cy.contains("button", "経路を生成").should("be.visible");
+  });
+
+  it("verify calendar functionality works correctly", () => {
+    cy.contains("footer ul li", "カレンダー").click();
+
+    const today = new Date();
+    const currentMonth = `${today.getFullYear()}年${today.getMonth() + 1}月`;
+    const prevMonth = `${today.getFullYear()}年${today.getMonth()}月`;
+
+    cy.contains("#calendar #title", currentMonth).should("be.visible");
+    cy.get("#calendar #prev").click();
+    cy.contains("#calendar #title", prevMonth).should("be.visible");
+    cy.get("#calendar .tbody").children().should("have.length.gte", 4);
   });
 });
