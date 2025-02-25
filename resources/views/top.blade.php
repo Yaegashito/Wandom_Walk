1fda00fe30f2ed24468ddfdd7356ad78465af57e
Yaegashito <tomoyae1007.xperia26@gmail.com>
2025-02-25 18:47:18 +0900

    その他画面完了

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -116,15 +116,6 @@
         </div>
 
         <div id="config" class="container">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <x-dropdown-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-                    {{ __('ログアウト') }}
-                </x-dropdown-link>
-            </form>
 
             <dl>
                 <div>
